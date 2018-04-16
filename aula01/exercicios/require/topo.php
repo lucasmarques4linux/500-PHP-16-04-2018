72b2766624443bff540c90ce142eb12f86042787
Lucas Marques <lucasmarques73@hotmail.com>
2018-04-16 17:19:29 -0300

    nav update

--- aula01/exercicios/require/topo.php
+++ aula01/exercicios/require/topo.php
@@ -5,7 +5,8 @@
 	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">	<ul class="navbar-nav">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<ul class="navbar-nav">
 		<li class = "nav-item"><a class ="nav-link"href="inicio.php">Inicio</a></li>
 		<li class = "nav-item"><a class ="nav-link"href="sobre.php">Sobre</a></li>
 		<li class = "nav-item"><a class ="nav-link"href="contato.php">Contato</a></li>
